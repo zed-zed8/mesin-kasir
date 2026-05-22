d47f8f7ead6e9e5c365dd377815067eabf725040
Ziddan <ziddanm45@gmail.com>
2026-05-22 15:29:20 +0700

    rewrite the keranjang table and making correlated changed

--- include/class/penjualan.php
+++ include/class/penjualan.php
@@ -7,23 +7,25 @@ class penjualan  extends database
         $data = mysqli_query($this->koneksi, 'select * from penjualan');
         return $data;
     }
+
     public function get_data_keranjang(int $id_penjualan): array
     {
         $keranjang = mysqli_query($this->koneksi, "SELECT * FROM keranjang WHERE id_penjualan='$id_penjualan'");
         $keranjang_belanja = [];
 
         foreach ($keranjang as $value) {
-            $id_barang = $value['id_barang'];
+            $nama_barang = $value['nama_barang'];
+            $harga_barang = $value['harga_barang'];
             $jumlah_barang = $value['jumlah_barang'];
 
-            $data_barang = mysqli_query(
-                $this->koneksi,
-                "SELECT * FROM barang WHERE id_barang ='$id_barang'"
-            );
-            foreach ($data_barang as $value2) {
-                $nama_barang = $value2['nama_barang'];
-                $harga_barang = $value2['harga_barang'];
-            }
+            // $data_barang = mysqli_query(
+            //     $this->koneksi,
+            //     "SELECT * FROM barang WHERE id_barang ='$id_barang'"
+            // );
+            // foreach ($data_barang as $value2) {
+            //     $nama_barang = $value2['nama_barang'];
+            //     $harga_barang = $value2['harga_barang'];
+            // }
 
             $keranjang_belanja[] = [
                 'nama_barang' => $nama_barang,
@@ -57,15 +59,25 @@ class penjualan  extends database
 
         $this->create_keranjang($keranjang, $id_penjualan_value);
     }
+
     public function create_keranjang(array $keranjang, int $id_penjualan): void
     {
         foreach ($keranjang as $value) {
             $id_barang = $value['id_barang'];
             $jumlah_barang = $value['jumlah_barang'];
 
+            $barang_data = mysqli_query(
+                $this->koneksi,
+                "SELECT * FROM barang WHERE id_barang = '$id_barang'"
+            );
+            foreach ($barang_data as $value2) {
+                $nama_barang = $value2['nama_barang'];
+                $harga_barang = $value2['harga_barang'];
+            }
+
             mysqli_query(
                 $this->koneksi,
-                "INSERT INTO keranjang VALUES (NULL, '$id_penjualan', '$id_barang', '$jumlah_barang')"
+                "INSERT INTO keranjang VALUES (NULL, '$id_penjualan', '$nama_barang', '$harga_barang', '$jumlah_barang')"
             );
         }
     }
